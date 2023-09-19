a349539c147a34c628f6fd5911403ca96f3ccb76
Ghali <nick_kali@yahoo.com>
2023-09-19 17:20:32 +0300

    1

--- projectG/predict_using_model_and_camera.js
+++ projectG/predict_using_model_and_camera.js
@@ -1,60 +1,61 @@
-/*
-DO THIS AFTER SAVING THE MODEL FROM PYTHON CODE
-Here we will load the model that we saved, and put the path for the model.
-*/
+// /*
+// DO THIS AFTER SAVING THE MODEL FROM PYTHON CODE
+// Here we will load the model that we saved, and put the path for the model.
+// */
 
-// Load the TensorFlow.js library
-// import * as tf from '@tensorflow/tfjs';
+// // Load the TensorFlow.js library
+// // import * as tf from '@tensorflow/tfjs';
 
-// Load the model
-let model;
-alpha = ["A","B","C","D","E","F","G","H","I","J",
-         "K","L","M","N","O","P","Q","R","S","T",
-          "V","W","X","Y","Z","??"];
-async function loadModel() {
-    model = await tf.loadLayersModel('content/model.json');
-}
-loadModel();
+// // Load the model
+// let model;
+// alpha = ["0","1","2", "3", "4", "5", "6", "7", "8", "9",
+//          "A","B","C","D","E","F","G","H","I","J",
+//          "K","L","M","N","O","P","Q","R","S","T",
+//           "V","W","X","Y","Z"];
+// async function loadModel() {
+//     model = await tf.loadLayersModel('./content/model.json');
+// }
+// loadModel();
 
-// Capture video from the webcam
-const video = document.getElementById('videoElement');
-navigator.mediaDevices.getUserMedia({ video: true })
-    .then(stream => {
-        video.srcObject = stream;
-    })
-    .catch(err => {
-        console.error("Error accessing the camera", err);
-    });
+// // Capture video from the webcam
+// const video = document.getElementById('videoElement');
+// navigator.mediaDevices.getUserMedia({ video: true })
+//     .then(stream => {
+//         video.srcObject = stream;
+//     })
+//     .catch(err => {
+//         console.error("Error accessing the camera", err);
+//     });
 
-function captureFrame() {
-    const canvas = document.createElement('canvas');
-    canvas.width = video.videoWidth;
-    canvas.height = video.videoHeight;
-    const ctx = canvas.getContext('2d');
-    ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
-    return tf.browser.fromPixels(canvas);
-}
+// function captureFrame() {
+//     const canvas = document.createElement('canvas');
+//     canvas.width = video.videoWidth;
+//     canvas.height = video.videoHeight;
+//     const ctx = canvas.getContext('2d');
+//     ctx.drawImage(video, 0, 0, canvas.width, canvas.height);
+//     return tf.browser.fromPixels(canvas);
+// }
 
-function preprocessFrame(frame) {
-    const resized = tf.image.resizeBilinear(frame, [224, 224]);
-    const normalized = resized.div(tf.scalar(255));
-    const batched = normalized.expandDims(0);
-    return batched;
-}
+// function preprocessFrame(frame) {
+//     const resized = tf.image.resizeBilinear(frame, [224, 224]);
+//     const normalized = resized.div(tf.scalar(255));
+//     const batched = normalized.expandDims(0);
+//     return batched;
+// }
 
-function handlePrediction(prediction) {
-    prediction.array().then(array => {
-        const maxIndex = array[0].indexOf(Math.max(...array[0]));
-        const resultElement = document.getElementById('result');
-        resultElement.textContent = `${alpha[maxIndex]} : الحرف الذي سيتم تمثيله سيظهر هنا`; 
-    });
-}
+// function handlePrediction(prediction) {
+//     prediction.array().then(array => {
+//         const maxIndex = array[0].indexOf(Math.max(...array[0]));
+//         const resultElement = document.getElementById('result');
+//         resultElement.textContent = `${alpha[maxIndex]} : الحرف الذي سيتم تمثيله سيظهر هنا`; 
+//     });
+// }
 
-function predict() {
-    const frame = captureFrame();
-    const preprocessedFrame = preprocessFrame(frame);
-    const prediction = model.predict(preprocessedFrame);
-    handlePrediction(prediction);
-}
+// function predict() {
+//     const frame = captureFrame();
+//     const preprocessedFrame = preprocessFrame(captureFrame);
+//     const prediction = model.predict(preprocessedFrame);
+//     handlePrediction(prediction);
+// }
 
-setInterval(predict, 100);
+// setInterval(predict, 100);
--- projectG/saving_model.py
+++ projectG/saving_model.py
@@ -3,7 +3,7 @@ from tensorflow import keras
 import tensorflowjs as tfjs
 
 # Load your pre-trained model (assuming it's a Keras model)
-model = keras.models.load_model('path_to_your_pretrained_model.h5')
+model = keras.models.load_model('./keras_model.h5')
 
 # Convert and save the model in TensorFlow.js format
-tfjs.converters.save_keras_model(model, 'path_to_save_tfjs_model/')
+tfjs.converters.save_keras_model(model, './content/model.json')
